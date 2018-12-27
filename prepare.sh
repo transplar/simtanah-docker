425755f473eb6f46c35d0ff7150ad5521eb40964
zackad <zackad.dev@gmail.com>
2018-12-27 18:34:59 +0700

    [cleanup] remove pictshare related content

--- prepare.sh
+++ prepare.sh
@@ -3,10 +3,6 @@
 # Pull submodule
 git submodule update --init --recursive
 
-# Mount volume of pictshare upload data
-mkdir -p var/pictshare_data
-chmod 777 var/pictshare_data
-
 # Install symfony app dependencies
 docker run --rm -it \
     --volume $PWD/modules/api:/app \
