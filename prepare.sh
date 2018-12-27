8a180684adb852666c3e109d0e0b570fefff52a3
zackad <zackad.dev@gmail.com>
2018-12-28 00:35:33 +0700

    [prepare] set permission upload directory

--- prepare.sh
+++ prepare.sh
@@ -3,6 +3,9 @@
 # Pull submodule
 git submodule update --init --recursive
 
+# Set permission
+chmod 777 modules/api/public/upload
+
 # Install symfony app dependencies
 docker run --rm -it \
     --volume $PWD/modules/api:/app \
